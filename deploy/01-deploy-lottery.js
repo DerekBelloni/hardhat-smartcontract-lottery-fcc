75ce2c7d6b0422d345264f7375ebbfab267947e1
Derek <derekbelloni10@gmail.com>
2023-01-14 20:58:19 -0700

    defined actions in deploy lottery script if testing on hardhat network

--- deploy/01-deploy-lottery.js
+++ deploy/01-deploy-lottery.js
@@ -1,13 +1,32 @@
-const { network } = require("hardhat");
+const { network, ethers } = require("hardhat");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address;
+    let subscriptionId;
 
+    if (developmentChains.includes(network.name)) {
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address;
+        const transactionResponse = await VRFCoordinatorV2Mock.createSubscription();
+        const transactionReceipt = await transactionResponse.wait(1);
+        subscriptionId = transactionReceipt.events[0].args.subId;
+        // now that we have a subscription, we need to fund the subscription;
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    }
+
+    const entrancFee = networkConfig[chainId]['entranceFee'];
+    const gasLane = networkConfig[chainId]["gasLane"];
+    const args = [vrfCoordinatorV2Address, entrancFee, gasLane]
     const lottery = await deploy("Lottery", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
-        waitConfirmations: network.config.blockConfirmations || 1,
+        // @ts-ignore
+        waitConfirmations = network.config.blockConfirmations || 1
     });
 };
